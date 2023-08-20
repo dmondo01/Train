674236083f5d849bb0d855f179a5dc34d959da58
Damien <damien.mondou@univ-lr.fr>
2023-08-20 15:35:33 +0200

    Add a tab to switch view

--- lib/view/homePage.dart
+++ lib/view/homePage.dart
@@ -1,9 +1,8 @@
-import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../api/request.dart';
-import '../model/station.dart';
-import '../model/train.dart';
+import 'stationView.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -16,99 +15,53 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final Request _request = Request();
-  List<Station> _stations = [];
-  List<Train> _departures = [];
+  int _selectedIndex = 0;
+  final List _pages = [
+    const Center(
+      child: StationView(title: 'My Train',),
+    ),
+    const Center(
+      //child: StationView(title: 'My Train',),
+    ),
+  ];
 
   @override
   void initState() {
-    _request.fetchStation().then((value) {
-      _stations = value;
-      //_request.readCsv().then((value) {
-        setState(() {
-          //_stations = value;
-        });
-      });
-    //});
     super.initState();
+    _request.fetchStation().then((value) {
+      setState(() {
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    GlobalKey<AutoCompleteTextFieldState<Station>> key = GlobalKey();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Train'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: AutoCompleteTextField<Station>(
-                key: key,
-                itemSubmitted: (item) {
-                  _request.getTrainsFromAPISNCF(item).then((value) async {
-                    _departures = value;
-                    for (var train in _departures) {
-                      await _request.getStops(train);
-                      setState(() { });
-                    }
-                  });
-                },
-                suggestions: _stations,
-                //link the list of stations to the autocomplete textfield
-                itemBuilder: (context, suggestion) => ListTile(
-                  //display the list of stations by their name
-                  title: Text(suggestion.name),
-                ),
-                itemFilter: (suggestion, query) {
-                  //search in the list of stations by their name
-                  return suggestion.name.toLowerCase().contains(query.toLowerCase());
-                },
-                itemSorter: (a, b) {
-                  //compare the stations by their name
-                  return a.name.compareTo(b.name);
-                },
-                clearOnSubmit: false,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'City',
-                ),
-              )
-            )
+      body:_pages[_selectedIndex],
+      bottomNavigationBar:
+      CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home, color: Colors.black),
+            label: 'Station',
           ),
-          Text(
-            'Next departures',
-            style: Theme.of(context).textTheme.titleMedium,
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.train_style_one, color: Colors.black),
+            label: 'Train',
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _departures.length,
-              itemBuilder: (context, index) {
-                final departure = _departures[index];
-                return Container(
-                  margin: const EdgeInsets.only(left: 20, right: 20, bottom: 0, top: 10),
-                  child: ListTile(
-                    title: Text("${departure.localHour}h${departure.localMinute} - ${departure.to?.station?.name}"),
-                    subtitle: Text("${departure.type?.name.replaceAll("_", " ")} ${departure.num}"),
-                    shape: const RoundedRectangleBorder(
-                      //borderRadius: BorderRadius.all(Radius.circular(32)),
-                      side: BorderSide(color: Colors.black),
-                    ),
-                    onTap: () {
-                      Navigator.pushNamed( context, '/map',
-                        arguments: departure,
-                      );
-                    },
-                  ),
-                );
-              }
-            ),
-          )
         ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  void _onItemTapped(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
   }
 }
---
+++ lib/view/stationView.dart
@@ -0,0 +1,111 @@
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:flutter/material.dart';
+
+import '../api/request.dart';
+import '../model/station.dart';
+import '../model/train.dart';
+
+class StationView extends StatefulWidget {
+  final String title;
+
+  const StationView({super.key, required this.title});
+
+  @override
+  State<StationView> createState() => _StationViewState();
+}
+
+class _StationViewState extends State<StationView> {
+  final Request _request = Request();
+  List<Station> _stations = [];
+  List<Train> _departures = [];
+
+  @override
+  void initState() {
+    _request.fetchStation().then((value) {
+      _stations = value;
+      //_request.readCsv().then((value) {
+      setState(() {
+        //_stations = value;
+      });
+    });
+    //});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<AutoCompleteTextFieldState<Station>> key = GlobalKey();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+              margin: const EdgeInsets.all(20),
+              child: Align(
+                  alignment: Alignment.topCenter,
+                  child: AutoCompleteTextField<Station>(
+                    key: key,
+                    itemSubmitted: (item) {
+                      _request.getTrainsFromAPISNCF(item).then((value) async {
+                        _departures = value;
+                        for (var train in _departures) {
+                          await _request.getStops(train);
+                          setState(() { });
+                        }
+                      });
+                    },
+                    suggestions: _stations,
+                    //link the list of stations to the autocomplete textfield
+                    itemBuilder: (context, suggestion) => ListTile(
+                      //display the list of stations by their name
+                      title: Text(suggestion.name),
+                    ),
+                    itemFilter: (suggestion, query) {
+                      //search in the list of stations by their name
+                      return suggestion.name.toLowerCase().contains(query.toLowerCase());
+                    },
+                    itemSorter: (a, b) {
+                      //compare the stations by their name
+                      return a.name.compareTo(b.name);
+                    },
+                    clearOnSubmit: false,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'City',
+                    ),
+                  )
+              )
+          ),
+          Text(
+            'Next departures',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: _departures.length,
+                itemBuilder: (context, index) {
+                  final departure = _departures[index];
+                  return Container(
+                    margin: const EdgeInsets.only(left: 20, right: 20, bottom: 0, top: 10),
+                    child: ListTile(
+                      title: Text("${departure.localHour}h${departure.localMinute} - ${departure.to?.station?.name}"),
+                      subtitle: Text("${departure.type?.name.replaceAll("_", " ")} ${departure.num}"),
+                      shape: const RoundedRectangleBorder(
+                        //borderRadius: BorderRadius.all(Radius.circular(32)),
+                        side: BorderSide(color: Colors.black),
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed( context, '/map',
+                          arguments: departure,
+                        );
+                      },
+                    ),
+                  );
+                }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
